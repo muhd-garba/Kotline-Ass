223d1ddd87b7505a66b43dfe646e6cbf47517430
Muhammad Garba Muhd <muhdgarba1038@gmail.com>
2025-05-10 20:52:35 +0100

    Question Four

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,17 @@
 fun main() {
-    print("Enter a number: ")
-    val number = readln().toInt()
+    // Print numbers 1 to 10
+    println("Numbers from 1 to 10:")
+    for (i in 1..10) {
+        print("$i ")
+    }
 
-    if (number % 2 == 0) {
-        println("$number is an even number.")
-    } else {
-        println("$number is an odd number.")
+    println("\n") // Add some space between outputs
+
+    // Print even numbers from 1 to 20
+    println("Even numbers from 1 to 20:")
+    for (i in 1..20) {
+        if (i % 2 == 0) {
+            print("$i ")
+        }
     }
 }
